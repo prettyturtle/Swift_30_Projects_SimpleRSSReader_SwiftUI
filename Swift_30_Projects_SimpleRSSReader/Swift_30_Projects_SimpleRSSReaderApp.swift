22ad0088f70709194ff23f48d12d65b51343b908
prettyturtle <unifconti@gmail.com>
2023-05-08 23:35:58 +0900

    Initial Commit

---
+++ Swift_30_Projects_SimpleRSSReader/Swift_30_Projects_SimpleRSSReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_30_Projects_SimpleRSSReaderApp.swift
+//  Swift_30_Projects_SimpleRSSReader
+//
+//  Created by yc on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct Swift_30_Projects_SimpleRSSReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
